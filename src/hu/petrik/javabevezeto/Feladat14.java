9fa2219e29542fb7a98a946072e8ae8680615efc
avpboy <somogyibotond03@gmail.com>
2022-09-13 14:36:48 +0200

    14. feladat

--- src/hu/petrik/javabevezeto/Feladat14.java
+++ src/hu/petrik/javabevezeto/Feladat14.java
@@ -10,10 +10,67 @@ public class Feladat14 {
         System.out.println("Kérem adjon meg egy szöveget:");
 
         String szoveg = sc.nextLine();
-        System.out.println("Mit szeretne tenni a szöveggel: ");
-        System.out.println("a - Nagy betűssé alakítani\nb - Kisbetűssé alakítani\nc - Lekérdezni a hosszát\n" +
-                "d - Összehasonlítani egy másik szöveggel\ne - Egy részét kiiratni a szövegnek\n" +
-                "f - Kilépnni");
-        System.out.println(szoveg);
+
+        boolean inLoop = true;
+
+        while (inLoop){
+            System.out.println("Mit szeretne tenni a szöveggel: ");
+            System.out.println("a - Nagy betűssé alakítani\nb - Kisbetűssé alakítani\nc - Lekérdezni a hosszát\n" +
+                    "d - Összehasonlítani egy másik szöveggel\ne - Egy részét kiiratni a szövegnek\n" +
+                    "f - Kilépnni");
+
+            String parancs = sc.nextLine();
+            System.out.println();
+            switch (parancs) {
+                case "a":
+                    System.out.println(upperCase(szoveg));
+                    break;
+                case "b":
+                    System.out.println(lowerCase(szoveg));
+                    break;
+                case "c":
+                    System.out.println(lenght(szoveg));
+                    break;
+                case "d":
+                    //Össze hasonlítani egy másik stringgel
+                    break;
+                case "e":
+                    System.out.println(specificString(szoveg));
+                    break;
+                case "f":
+                    inLoop = false;
+                    break;
+                default:
+                    System.out.println("Hiba");
+                    break;
+            }
+
+        }
+        System.out.println("\nKilépve\n");
+
+
     }
+
+    static String upperCase(String szoveg) {
+        return szoveg.toUpperCase();
+    }
+
+    static  String lowerCase(String szoveg) {
+        return szoveg.toLowerCase();
+    }
+
+    static int lenght(String szoveg) {
+        return szoveg.length();
+    }
+
+    static String specificString(String szoveg) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Adja meg mettől: ");
+        int a = sc.nextInt();
+        System.out.print("Adja meg meddig: ");
+        int b = sc.nextInt();
+        return szoveg.substring(a, b);
+    }
+
+
 }
